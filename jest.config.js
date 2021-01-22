2fc2d84eac373645886a2be537e44024c07cade4
vmo-campos <viniciusmoliveirac@hotmail.com>
2021-01-22 18:43:15 -0300

    chore: add path module find

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,16 @@
-export default {
+module.exports = {
   roots: ['<rootDir>/tests'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
   }
 }
