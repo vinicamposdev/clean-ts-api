7c77db9652817afc51bb15680f382bd97faeb6cb
vmo-campos <viniciusmoliveirac@hotmail.com>
2021-01-22 22:36:52 -0300

    refactor: ensure Signcontroller.handle implement correct interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,7 @@
+import { IHttpRequest, IHttpResponse } from '@/protocols/http'
+
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: IHttpRequest): IHttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
