beb7d14b799dc5267160d990cf210075d91be87c
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-05-16 12:58:35 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ tests/presentation/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,9 @@
+import { EmailValidatorAdapter } from '@/presentation/utils/email-validator-adapter'
+
+describe('EmailValidator Adapter', () => {
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email.@mail.com')
+    expect(isValid).toBe(false)
+  })
+})
