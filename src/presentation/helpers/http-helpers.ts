e57ec8568be72fb85518e70dac24328667230540
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-05-16 12:25:05 -0300

    refactor: change import

--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -1,4 +1,4 @@
-import { ServerError } from '@/presentation/errors/server-error'
+import { ServerError } from '@/presentation/errors'
 import { IHttpResponse } from '@/presentation/protocols/http'
 
 export const badRequest = (error: Error): IHttpResponse => ({
