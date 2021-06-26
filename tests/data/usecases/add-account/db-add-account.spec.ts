a66e1effa9a98760afe892060193a2b57f0753cf
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-06-26 09:23:39 -0300

    test: ensure DbAddAccount returns an account on success

--- tests/data/usecases/add-account/db-add-account.spec.ts
+++ tests/data/usecases/add-account/db-add-account.spec.ts
@@ -106,4 +106,22 @@ describe('DbAddAccount Usecase', () => {
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
   })
+
+  test('Shoud return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const account = await sut.add(accountData)
+
+    await expect(account).toEqual({
+      id: 'valid_id',
+      ...accountData,
+      password: 'hashed_password'
+    })
+  })
 })
