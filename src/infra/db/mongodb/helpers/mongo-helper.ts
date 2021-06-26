31d3a052c348e1d93b35e65af5dcfd8cc708ebad
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-06-26 15:23:10 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,20 @@
+import { Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  }
+}
