e7d56b2b98fde72c81a577fa59ea5ddddfd35f05
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-06-27 19:58:39 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,9 @@
-import app from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 
-app.listen(5050, () => console.log('Server running at 5050'))
+import env from './config/env'
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server running at ${env.port}`))
+  })
+  .catch(console.error)
