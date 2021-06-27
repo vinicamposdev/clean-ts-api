21e12d219ba6dab6737998eded8c0bb06f516e5f
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-06-27 19:26:18 -0300

    feat: add signup routes

---
+++ src/main/config/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import fg from 'fast-glob'
+
+export default (app: Router): void => {
+  const router = Router()
+  app.use('/api', router)
+
+  fg.sync('**/src/main/routes/**routes.ts')
+    .map(async file => (await import(`../../../${file}`)).default(router))
+}
