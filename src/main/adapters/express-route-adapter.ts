58a1b3556e0269ccfdb8ecd9bb8eb6ddb0b83c51
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-06-28 00:00:39 -0300

    refactor: move route inicialization to an adapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,16 @@
+import 'module-alias/register'
+import { IController, IHttpRequest } from '@/presentation/protocols'
+import { Request, Response } from 'express'
+
+export const adaptRoute = (controller: IController) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: IHttpRequest = {
+      body: req.body
+    }
+    console.log({ httpRequest })
+
+    const httpResponse = await controller.handle(httpRequest)
+    console.log({ httpResponse })
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
+import { makeSignUpController } from '../factories/signup'
+import { adaptRoute } from '@/main/adapters/express-route-adapter'
 
 export default (router: Router): void => {
-  router.post('/signup', (req, res) => {
-    res.json({ ok: 'ok' })
-  })
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  router.post('/signup', adaptRoute(makeSignUpController()))
 }
