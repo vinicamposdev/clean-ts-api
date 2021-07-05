48df84587145cae85907eab52153d5972e2167b2
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-05 19:49:33 -0300

    test: ensure LogControllerDecorator returns correct value on success

--- tests/main/decorator/log.spec.ts
+++ tests/main/decorator/log.spec.ts
@@ -48,4 +48,25 @@ describe('LogController Decorator', () => {
 
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_mail@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'any_name'
+      }
+    })
+  })
 })
