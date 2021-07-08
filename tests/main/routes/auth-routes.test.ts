dc553624ca3cedce6fd9e1331414555d3b28217f
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-08 14:59:30 -0300

    test: add login route

--- tests/main/routes/auth-routes.test.ts
+++ tests/main/routes/auth-routes.test.ts
@@ -1,6 +1,10 @@
+import { hash } from 'bcrypt'
 import request from 'supertest'
 import app from '@/main/config/app'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+
+let accountCollection: Collection
 
 describe('Auth Routes', () => {
   beforeAll(async () => {
@@ -8,7 +12,7 @@ describe('Auth Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -24,6 +28,25 @@ describe('Auth Routes', () => {
           email: 'valid_email@mail.com',
           password: 'valid_password',
           passwordConfirmation: 'valid_password'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login success', async () => {
+      const password = await hash('valid_password', 12)
+      await accountCollection.insertOne({
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password
+      })
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'valid_email@mail.com',
+          password: 'valid_password'
         })
         .expect(200)
     })
