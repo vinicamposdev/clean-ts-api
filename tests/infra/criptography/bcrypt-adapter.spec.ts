56d2e289516062b4db19a7f09b1e80206d17b882
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-08 08:42:24 -0300

    test: ensure Bcrypt throw if compare throws

--- tests/infra/criptography/bcrypt-adapter.spec.ts
+++ tests/infra/criptography/bcrypt-adapter.spec.ts
@@ -38,7 +38,7 @@ describe('Bcrypt Adpter', () => {
     expect(hash).toBe('hash')
   })
 
-  test('Should throw if bcrypt throws', async () => {
+  test('Should throw if hash throws', async () => {
     const sut = makeSut()
 
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
@@ -67,5 +67,30 @@ describe('Bcrypt Adpter', () => {
     const isValid = await sut.compare('any_value', 'any_hash')
 
     expect(isValid).toBe(true)
+  })
+
+  test('Should return false when compare fails', async () => {
+    const sut = makeSut()
+
+    // jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(
+    //   new Promise(resolve => resolve(false))
+    // )
+
+    const isValid = await sut.compare('any_value', 'any_hash')
+
+    expect(isValid).toBe(!false)
+  })
+
+  test('Should throw if compare throws', async () => {
+    const sut = makeSut()
+
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    jest.spyOn(bcrypt, 'compare').mockImplementation(async () => {
+      return await new Promise((_resolve, reject) => reject(new Error()))
+    })
+
+    const promise = sut.compare('any_value', 'any_hash')
+
+    await expect(promise).rejects.toThrow()
   })
 })
