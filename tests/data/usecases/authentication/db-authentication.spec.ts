3ddebd702126c3faf7ea08a76a17b53d2e9aa6f4
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-08 07:53:48 -0300

    test: ensure DbAuthentication  throw if UpdateAccessTokenRepository throws

--- tests/data/usecases/authentication/db-authentication.spec.ts
+++ tests/data/usecases/authentication/db-authentication.spec.ts
@@ -186,4 +186,16 @@ describe('DbAuthentication UseCase', () => {
 
     expect(updateSpy).toHaveBeenCalledWith('any_id', 'any_token')
   })
+
+  test('Should throw if UpdateAccessTokenRepository throws', async () => {
+    const { sut, updateAccessTokenRepositoryStub } = makeSut()
+
+    jest.spyOn(updateAccessTokenRepositoryStub, 'update').mockReturnValueOnce(
+      new Promise((resolve, reject) => reject(new Error()))
+    )
+
+    const promise = sut.auth(makeFakeAuthentication())
+
+    await expect(promise).rejects.toThrow()
+  })
 })
