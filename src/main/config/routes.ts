4e0ec79c152e73f715fcf30f649ea7e106b5c5b0
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-13 10:45:16 -0300

    feat: map dir routes only over js files to build project

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
-import fg from 'fast-glob'
+import { readdirSync } from 'fs'
 
 export default (app: Router): void => {
   const router = Router()
   app.use('/api', router)
 
-  fg.sync('**/src/main/routes/**routes.ts')
-    .map(async file => (await import(`../../../${file}`)).default(router))
+  readdirSync(`${__dirname}/../routes`).map(async file => {
+    if (!file.includes('.test.') && !file.endsWith('.map') && !file.includes('.spec.')) {
+      (await import(`../routes/${file}`)).default(router)
+    }
+  })
 }
