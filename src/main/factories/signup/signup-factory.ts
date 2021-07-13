e9aff315e75f23a613da7f326d19259ec5a395fd
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-13 14:20:28 -0300

    feat: ensure SignUp Factory intanciate SignUp controller with Authentication

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -6,14 +6,19 @@ import { SignUpController } from '@/presentation/controllers/signup/signup-contr
 import { IController } from '@/presentation/protocols'
 import { LogControllerDecorator } from '@/main/decorators/log-controller-decorator'
 import { makeSignUpValidation } from './signup-validation-factory'
+import { JwtAdapter } from '@/infra/criptography/jwt-adapter/jwt-adapter'
+import { DbAuthentication } from '@/data/usecases/authentication/db-authentication'
+import env from '@/main/config/env'
 
 export const makeSignUpController = (): IController => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const dbAuthentication = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
   const logMongoRepository = new LogMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
+  const signUpController = new SignUpController(dbAddAccount, dbAuthentication, makeSignUpValidation())
 
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }
