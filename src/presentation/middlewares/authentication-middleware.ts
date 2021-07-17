72eb282fecaf472cd657a72ce9ed693416970960
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-17 07:01:25 -0300

    feat: ensure Authentication Middleware call LoadAccountByToken with role if exists

--- src/presentation/middlewares/authentication-middleware.ts
+++ src/presentation/middlewares/authentication-middleware.ts
@@ -5,14 +5,15 @@ import { IHttpRequest, IHttpResponse, IMiddleware } from '@/presentation/protoco
 
 export class AuthenticationMiddleware implements IMiddleware {
   constructor (
-    private readonly loadAccountByToken: ILoadAccountByToken
+    private readonly loadAccountByToken: ILoadAccountByToken,
+    private readonly role = 'default'
   ) {}
 
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
     try {
       const accessToken = httpRequest.headers?.['x-access-token']
       if (accessToken) {
-        const account = await this.loadAccountByToken.load(accessToken)
+        const account = await this.loadAccountByToken.load(accessToken, this.role)
         if (account) {
           return ok({ accountId: account.id })
         }
