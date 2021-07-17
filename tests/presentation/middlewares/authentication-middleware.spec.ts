13603e1f8fe30477519bc48e68ece0184c9e9b38
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-17 06:42:10 -0300

    test: ensure Authentication Middleaware return 403 if LoadAccountByToken returns null

--- tests/presentation/middlewares/authentication-middleware.spec.ts
+++ tests/presentation/middlewares/authentication-middleware.spec.ts
@@ -56,4 +56,11 @@ describe('Authentication Middleware', () => {
 
     expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
+
+  test('Should return 403 if LoadAccountByToken returns null', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    jest.spyOn(loadAccountByTokenStub, 'load').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(forbiden(new AccessDeniedError()))
+  })
 })
