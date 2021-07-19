320904a52eb9f4b3c07e77fddf8e8c8e0b3a5a13
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-19 11:56:22 -0300

    feat: ensure LoadSurveysController returns 200 on success

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,10 +1,11 @@
 import { ILoadSurveys } from '@/domain/usecases/load-surveys'
+import { ok } from '@/presentation/middlewares/authentication-middleware-protocols'
 import { IController, IHttpRequest, IHttpResponse } from '@/presentation/protocols'
 
 export class LoadSurveysController implements IController {
   constructor (private readonly loadSurveys: ILoadSurveys) {}
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    await this.loadSurveys.load()
-    return null
+    const surveys = await this.loadSurveys.load()
+    return ok(surveys)
   }
 }
