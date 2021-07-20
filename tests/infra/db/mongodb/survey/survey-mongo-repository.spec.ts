74fd68d2cf426536c181b8d2d8826d161a439ccd
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-20 18:57:03 -0300

    test: ensure SurveyMongoRepository load empty list if there are no surveys

--- tests/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -1,6 +1,20 @@
+import MockDate from 'mockdate'
+
+import { IAddSurveyModel } from '@/domain/usecases/add-survey'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import { SurveyMongoRepository } from '@/infra/db/mongodb/survey/survey-mongo-repository'
 import { Collection } from 'mongodb'
+
+const makeFakeSurvey = (): IAddSurveyModel => ({
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }, {
+    answer: 'any_other_answer'
+  }],
+  date: new Date()
+})
 
 let surveyCollection: Collection
 
@@ -10,6 +24,7 @@ const makeSut = (): SurveyMongoRepository => {
 
 describe('Survey Mongo Repository', () => {
   beforeAll(async () => {
+    MockDate.set(new Date())
     await MongoHelper.connect(process.env.MONGO_URL)
   })
 
@@ -19,24 +34,38 @@ describe('Survey Mongo Repository', () => {
   })
 
   afterAll(async () => {
+    MockDate.reset()
     await MongoHelper.disconnect()
   })
 
-  test('Should add a new survey on add success', async () => {
-    const sut = makeSut()
+  describe('add()', () => {
+    test('Should add a new survey on add success', async () => {
+      const sut = makeSut()
 
-    await sut.add({
-      question: 'any_question',
-      answers: [{
-        image: 'any_image',
-        answer: 'any_answer'
-      }, {
-        answer: 'any_other_answer'
-      }],
-      date: new Date()
+      await sut.add(makeFakeSurvey())
+
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
+    })
+  })
+
+  describe('loadAll()', () => {
+    test('Should load all surveys on success', async () => {
+      await surveyCollection.insertMany([makeFakeSurvey(), makeFakeSurvey()])
+      const sut = makeSut()
+
+      const surveys = await sut.loadAll()
+
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
     })
 
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
+    test('Should load empty list', async () => {
+      const sut = makeSut()
+
+      const surveys = await sut.loadAll()
+
+      expect(surveys.length).toBe(0)
+    })
   })
 })
