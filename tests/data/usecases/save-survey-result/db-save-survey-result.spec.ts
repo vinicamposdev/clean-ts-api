378ddab1238a475ff4d8dfc2eba2eeb9872a1b0e
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-24 10:10:45 -0300

    test: ensure DbSaveSurveyResult return a survey on success

--- tests/data/usecases/save-survey-result/db-save-survey-result.spec.ts
+++ tests/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -62,4 +62,10 @@ describe('DbSaveSurveyResult UseCase', () => {
     const promise = sut.save(makeFakeSurveyResultData())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return a survey on success', async () => {
+    const { sut } = makeSut()
+    const surveys = await sut.save(makeFakeSurveyResultData())
+    expect(surveys).toEqual(makeFakeSurveyResult())
+  })
 })
