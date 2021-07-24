56043dea867173bbc7427bdda3580db8e05c3f7b
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-24 11:25:11 -0300

    test: ensure Save Survey Result Mongo Repository add a new survey on save success

---
+++ tests/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -0,0 +1,79 @@
+import MockDate from 'mockdate'
+
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { SurveyResultMongoRepository } from '@/infra/db/mongodb/survey-result/survey-result-mongo-repository'
+import { Collection } from 'mongodb'
+import { SurveyModel } from '@/domain/models/survey'
+import { IAccountModel } from '@/domain/models/account'
+
+let surveyCollection: Collection
+let surveyResultCollection: Collection
+let accountCollection: Collection
+
+const makeSurvey = async (): Promise<SurveyModel> => {
+  const res = await surveyCollection.insertOne({
+    question: 'any_question',
+    answers: [{
+      image: 'any_image',
+      answer: 'any_answer_1'
+    }, {
+      answer: 'any_answer_2'
+    }, {
+      answer: 'any_answer_3'
+    }],
+    date: new Date()
+  })
+
+  return res.ops[0]
+}
+const makeAccount = async (): Promise<IAccountModel> => {
+  const res = await accountCollection.insertOne({
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password'
+  })
+
+  return res.ops[0]
+}
+
+const makeSut = (): SurveyResultMongoRepository => {
+  return new SurveyResultMongoRepository()
+}
+
+describe('Save Survey Result Mongo Repository', () => {
+  beforeAll(async () => {
+    MockDate.set(new Date())
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    await surveyResultCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    MockDate.reset()
+    await MongoHelper.disconnect()
+  })
+
+  describe('save()', () => {
+    test('Should add a survey result if its new', async () => {
+      const survey = await makeSurvey()
+      const account = await makeAccount()
+      const sut = makeSut()
+      const surveyResult = await sut.save({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[0].answer,
+        date: new Date()
+      })
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.id).toBeTruthy()
+      expect(surveyResult.answer).toBe(survey.answers[0].answer)
+    })
+  })
+})
