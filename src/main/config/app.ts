d0f43a15b2aa7b9007732536aac0c39cbaf51b1d
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-25 04:26:03 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
+import setupSwagger from './config-swagger'
 
 const app = express()
 
+setupSwagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
 
---
+++ src/main/docs/index.ts
@@ -0,0 +1,32 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'API de Enquetes',
+    description: 'Essa é a documentação da API feita no curso do instrutor da Udemy, Rodrigo Manguinho, de NodeJs usando Typescript, TDD, Clean Architecture e seguindo os princípios do SOLID e Design Patterns.',
+    version: '1.0.0',
+    contact: {
+      name: 'Vinicius Campos',
+      email: 'vinic.otus@gmail.com',
+      url: 'https://www.linkedin.com/in/vinicamposdev'
+    },
+    license: {
+      name: 'GPL-3.0-or-later',
+      url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
+    }
+  },
+  externalDocs: {
+    description: 'Link para o treinamento completo',
+    url: 'https://www.udemy.com/course/tdd-com-mango'
+  },
+  servers: [{
+    url: '/api',
+    description: 'Servidor Principal'
+  }],
+  tags: [{
+    name: 'Login',
+    description: 'APIs relacionadas a Login'
+  }, {
+    name: 'Enquete',
+    description: 'APIs relacionadas a Enquete'
+  }]
+}
