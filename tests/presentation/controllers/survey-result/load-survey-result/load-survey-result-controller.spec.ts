3b296c3878b84d891727193ef3f7c436ea3201ff
vinicamposdev <viniciusmoliveirac@hotmail.com>
2021-11-30 13:48:40 -0300

    test: ensure DbLoadSurveyResult throws if LoadSurveyResult throws

--- tests/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ tests/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -67,4 +67,11 @@ describe('SaveSurveyResultController', () => {
     await sut.handle(makeFakeRequest())
     expect(loadSpy).toHaveBeenCalledWith('any_survey_id')
   })
+
+  test('Should return 500 if LoadSurveyResult throws', async () => {
+    const { loadSurveyResultStub, sut } = makeSut()
+    jest.spyOn(loadSurveyResultStub, 'load').mockImplementationOnce(throwError)
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
