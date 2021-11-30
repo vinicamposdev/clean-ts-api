56c1ae8e0e0951cd9df05056d4dba2089b8207a6
vinicamposdev <viniciusmoliveirac@hotmail.com>
2021-11-30 15:54:55 -0300

    test: change type to namespace type in models

--- tests/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -141,5 +141,12 @@ describe('SurveyResultMongoRepository', () => {
       expect(surveyResult.answers[2].count).toBe(0)
       expect(surveyResult.answers[2].percent).toBe(0)
     })
+
+    test('Should load survey result', async () => {
+      const survey = await makeSurvey()
+      const sut = makeSut()
+      const surveyResult = await sut.loadBySurveyId(survey.id)
+      expect(surveyResult).toBeNull()
+    })
   })
 })
